d496520120eb971af5d94bfdca2b46cf42857194
Sayf Ismail <sayfti@gmail.com>
2020-12-02 22:05:41 +0800

    added Login page.

--- main.rb
+++ main.rb
@@ -5,6 +5,32 @@ require 'pg'
 require 'pry'
 require 'bcrypt'
 
+enable :sessions
+
+def user_found(users)
+  if users.to_a.length > 0
+    users[0]
+  else
+    nil
+  end
+end
+
+def logged_in?
+  !!session[:user_id]
+end
+
+def current_user
+  if logged_in?
+    user_id = session[:user_id]
+    users = run_sql("SELECT * FROM users WHERE id=#{user_id}")
+    # This runsql will give us a collection of records, specifically data from PG
+    user = user_found(users)
+    return user
+  else
+    nil
+  end
+end
+
 def run_sql(sql)
   db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'appreciation_app_db'})
   results = db.exec(sql)
@@ -38,3 +64,33 @@ post '/users' do
   redirect '/'
 end
 
+get '/login' do
+  erb :'/sessions/login', locals: {
+      error_message: ''
+  }
+end
+
+post '/sessions' do
+  user_email = params['user_email']
+  user_password = params['password']
+
+  users = run_sql("SELECT * FROM users WHERE email='#{user_email}'")
+  user = user_found(users)
+
+  if BCrypt::Password.new(user['password_digest']) == params['password']
+    # Log the user in
+    session[:user_id] = user['id']
+
+    redirect '/'
+  else
+    erb :'/sessions/login', locals: {
+      error_message: "incorrect login"
+  }
+  end
+end
+
+delete '/sessions' do
+  session[:user_id] = nil  
+
+  redirect '/'
+end
