7b39c7a395cc7d277965fe6ead4f947adb0cd8a0
Sayf Ismail <sayfti@gmail.com>
2020-12-02 16:08:36 +0800

    added wall posts to test database request.

--- main.rb
+++ main.rb
@@ -7,13 +7,13 @@ require 'bcrypt'
 
 def run_sql(sql)
   db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'appreciation_app_db'})
-  result = db.exec(sql)
+  results = db.exec(sql)
   db.close
-  return result
+  return results
 end
 
 get '/' do
-  posts = run_sql("SELECT * FROM posts ORDER BY ID")
+  posts = run_sql("SELECT * FROM posts")
   erb :'/wallposts/index', locals: {
     posts: posts
   }
--- schema.sql
+++ schema.sql
@@ -2,4 +2,8 @@ CREATE DATABASE appreciation_app_db;
 
 CREATE TABLE posts(id SERIAL PRIMARY KEY, giver TEXT, receiver TEXT, message TEXT, timestamp TIMESTAMP, image_url TEXT);
 
+CREATE TABLE users(id SERIAL PRIMARY KEY, user_handle TEXT, email TEXT, user_image_URL TEXT, password_digest TEXT);
+
 INSERT INTO posts(giver, receiver, message, image_url) VALUES('jonsnow', 'danytarg', 'Thanks for saving me from the undead at that lake with all those dragons and stuff...sorry one died!', 'https://i.imgur.com/fTcxtZO.jpg');
+
+INSERT INTO posts(giver, receiver, message, timestamp, image_url) VALUES('voldemort', 'harryp', 'Join me! We are the same, you and I... :D', current_timestamp, 'https://i.imgur.com/dsuEYpS.jpg');
